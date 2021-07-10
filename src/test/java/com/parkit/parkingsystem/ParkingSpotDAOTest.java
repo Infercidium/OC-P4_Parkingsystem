f661936bcbea1c01c132c91b1f4b35109a43a873
Maxime Vautier <vautier_maxime@yahoo.com>
2021-07-10 18:27:40 +0200

    Correction CheckStyle, FindBugs, PMD. V3 + Ajout Test.

--- src/test/java/com/parkit/parkingsystem/ParkingSpotDAOTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingSpotDAOTest.java
@@ -2,36 +2,24 @@ package com.parkit.parkingsystem;
 
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
-import com.parkit.parkingsystem.dao.TicketDAO;
 import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
-import com.parkit.parkingsystem.integration.service.DataBasePrepareService;
 import com.parkit.parkingsystem.model.ParkingSpot;
-import com.parkit.parkingsystem.model.Ticket;
-import com.parkit.parkingsystem.service.FareCalculatorService;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class ParkingSpotDAOTest {
 
-        private static DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
+        private static final DataBaseTestConfig dataBaseTestConfig = new DataBaseTestConfig();
         private static ParkingSpotDAO parkingSpotDAO;
-        private static Ticket ticket;
-        private static DataBasePrepareService dataBasePrepareService;
-        private FareCalculatorService fcs = new FareCalculatorService();
 
-        @BeforeAll
+    @BeforeAll
         private static void setUp() {
             parkingSpotDAO = new ParkingSpotDAO();
             parkingSpotDAO.setDataBaseConfig(dataBaseTestConfig);
-            ticket = new Ticket();
-            FareCalculatorService fcs = new FareCalculatorService();
-        }
+    }
 
         @Test
         public void getNextAvailableSlotTest() {
